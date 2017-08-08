1e7fd6a24944a2ce42be01af23a09561c716ae86
C2P1 <C2P1@users.noreply.github.com>
2017-08-08 17:27:25 +0100

    Add Sentence.java

---
+++ Week 1 - Core Data Structures/Homework 3 - Collections/Sentence.java
@@ -0,0 +1,48 @@
+/*
+ * SD2x Homework #3
+ * This class represents a single sentence.
+ */
+
+public class Sentence implements Comparable<Sentence> {
+	protected int score;
+	protected String text;
+	
+	public Sentence(int score, String text) {
+		this.score = score;
+		this.text = text;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public String getText() {
+		return text;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sentence other = (Sentence) obj;
+		if (score != other.score)
+			return false;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(Sentence o) {
+		return this.score - o.score;
+	}
+	
+	
+}
