d6c4d42cc085b17ca80d2663d8ae53476be7804a
C2P1 <C2P1@users.noreply.github.com>
2017-08-11 16:24:51 +0100

    Create UserMovieRating.java

---
+++ Week 2 - Advanced Data Structures/Homework 5/UserMovieRating.java
@@ -0,0 +1,46 @@
+/*
+ * SD2x Homework #5
+ */
+
+public class UserMovieRating implements Comparable<UserMovieRating> {
+
+	protected String movie;
+	protected int userRating;
+	
+	public UserMovieRating(String movie, int userRating) {
+		this.movie = movie;
+		this.userRating = userRating;
+	}
+	
+	public String getMovie() {
+		return movie;
+	}
+	
+	public int getUserRating() {
+		return userRating;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserMovieRating other = (UserMovieRating) obj;
+		if (movie == null) {
+			if (other.movie != null)
+				return false;
+		} else if (!movie.equals(other.movie))
+			return false;
+		if (userRating != other.userRating)
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(UserMovieRating other) {
+		return this.userRating - other.userRating;
+	}	
+}
